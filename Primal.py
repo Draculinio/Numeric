fcb1ddc7cd3a646e601b7d6df9ad83da8e3f9cb2
Pablo Sebastián Soifer <stackpointerex@gmail.com>
2016-09-10 10:21:22 -0300

    Primera versión

---
+++ Primal.py
@@ -0,0 +1,23 @@
+import datetime
+import time
+
+def definirPrimalidad(num):
+    primo = True
+    for i in range(3, num):
+        if num != 2 or num != 3:
+            if num % i == 0:
+                primo = False
+                break
+    return primo
+
+num = int(input("Ingrese un número: "))
+print ("Tiempo de inicio: "+str(datetime.datetime.now().time()))
+inicio = time.time()
+primo = definirPrimalidad(num)
+final = time.time()
+if primo == True:
+    print("Es primo")
+else:
+    print("No es primo")
+print ("Tiempo de fin: "+str(datetime.datetime.now().time()))
+print  ("Tiempo de ejecución: "+str(final-inicio))
